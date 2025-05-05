ddd94b84a7d4b0bc81f9621346870fbe444feda8
DiegoRamirezFuente <dramirezfuente@gmail.com>
2025-05-05 19:27:36 +0200

    INterfaz ajustada a Ubuntu

--- FinalCode/gui_imagen.py
+++ FinalCode/gui_imagen.py
@@ -11,7 +11,7 @@ valores = {
     'box2': {'valor': 200, 'x': 1010, 'y': 425},
     'box3': {'valor': 300, 'x': 1455, 'y': 185},
     'box4': {'valor': 400, 'x': 1455, 'y': 425},
-    'boxColor': {'valor': 'Red', 'x': 240, 'y': 710}
+    'boxColor': {'valor': 'Green', 'x': 240, 'y': 710}
 }
 
 # ---- LABEL CLICKEABLE ----
@@ -58,11 +58,13 @@ class MainScreen(QWidget):
 
     def set_cap_info(self, centroide: Tuple[int, int], bounding_box: Tuple[int, int, int, int], color: str):
         print(f"[INFO] Recibido centroide: {centroide}, bounding box: {bounding_box}, color: {color}")
-
-        self.box1.setText("100")
-        self.box2.setText("200")
+        
+        self.box1.setText("0")
+        self.box2.setText("0")
+        self.box3.setText("0")
+        self.box4.setText("0")
         self.boxColor.setText(color)
-        self.boxCoord.setText(f"({centroide[0]}, {centroide[1]})")
+        self.boxCoord.setText(f"({centroide[0]}, {centroide[1]}) px")
 
         # 1. Cargar imagen original
         imagen = cv2.imread("tapones3.jpg")
@@ -101,30 +103,54 @@ class MainScreen(QWidget):
         self.camera_label.setPixmap(QPixmap.fromImage(qt_img))
 
     def create_value_boxes(self):
+        # Fuente base
         font = QFont()
-        font.setPointSize(20)
-        for box_name, config in valores.items():
+        
+        # Estilo para box1 a box4
+        style_boxes = """
+            background-color: transparent;
+            border: none;
+            font-size: 50px;
+            color: black;
+        """
+        font.setPointSize(45)
+
+        for box_name in ['box1', 'box2', 'box3', 'box4']:
+            config = valores[box_name]
             box = QLineEdit(self)
             box.setText(str(config['valor']))
             box.setReadOnly(True)
-            box.setGeometry(config['x'], config['y'], 100, 80)
-            box.setStyleSheet("""
-                background-color: white;
-                border: 2px solid black;
-                font-size: 50px;
-            """)
+            box.setGeometry(config['x'], config['y'], 120, 80)  # mismo tamaño para todos
+            box.setStyleSheet(style_boxes)
             box.setFont(font)
             setattr(self, box_name, box)
 
+        # Estilo distinto para boxColor y boxCoord
+        style_extra = """
+            background-color: transparent;
+            border: none;
+            font-size: 30px;
+            color: black;
+        """
+        font_extra = QFont()
+        font_extra.setPointSize(30)
+
+        # boxColor
+        config = valores['boxColor']
+        self.boxColor = QLineEdit(self)
+        self.boxColor.setText(config['valor'])
+        self.boxColor.setReadOnly(True)
+        self.boxColor.setGeometry(config['x'], config['y'], 200, 80)
+        self.boxColor.setStyleSheet(style_extra)
+        self.boxColor.setFont(font_extra)
+
+        # boxCoord
         self.boxCoord = QLineEdit(self)
         self.boxCoord.setReadOnly(True)
-        self.boxCoord.setGeometry(valores['boxColor']['x'], valores['boxColor']['y'] + 90, 200, 80)
-        self.boxCoord.setStyleSheet("""
-            background-color: white;
-            border: 2px solid black;
-            font-size: 30px;
-        """)
-        self.boxCoord.setFont(font)
+        self.boxCoord.setGeometry(290, 815, 200, 80)
+        self.boxCoord.setStyleSheet(style_extra)
+        self.boxCoord.setFont(font_extra)
+
 
     def paintEvent(self, event):
         painter = QPainter(self)
